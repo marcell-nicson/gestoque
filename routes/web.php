b83a4204fe8c3a5b6f043f05177e6645703d16c2
Marcell Nicson <marcell.nicson@gmail.com>
2024-09-15 18:38:52 -0300

    a

--- routes/web.php
+++ routes/web.php
@@ -38,11 +38,11 @@ Route::middleware('auth')->group(function () {
         Route::put('produtos/{id}', [ProdutoController::class, 'update'])->name('produtos.update'); 
         Route::delete('destroy/{id}', [ProdutoController::class, 'destroy'])->name('produtos.destroy');
 
-        Route::post('/produto/reenviar/{id}', function($id) {
+        Route::post('/produtos/reenviar/{id}', function($id) {
             $produto = \App\Models\Produto::find($id);
             event(new ProdutoCriado($produto));
             return redirect()->back()->with('success', 'Evento disparado com sucesso!');
-        })->name('produto.reenviar');
+        })->name('produtos.reenviar');
         
         Route::post('ofertas/storeOferta', [OfertaController::class, 'storeOferta'])->name('ofertas.storeOferta');
 
