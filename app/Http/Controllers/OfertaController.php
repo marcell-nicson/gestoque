ecd100232a551f74c0f3884ad48c89e2161017e2
Marcell Nicson <marcell.nicson@gmail.com>
2024-09-25 15:32:53 -0300

    uauauau

--- app/Http/Controllers/OfertaController.php
+++ app/Http/Controllers/OfertaController.php
@@ -6,10 +6,12 @@ use App\Events\ProdutoCriado;
 use App\Http\Requests\StoreOfertaRequest;
 use App\Models\Grupo;
 use App\Models\Produto;
+use App\Services\EvolutionApi;
 use App\Services\OfertaService;
 use App\Services\Uzapi;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class OfertaController extends Controller
 {
@@ -46,10 +48,24 @@ class OfertaController extends Controller
             if ($produto) {
 
                 $grupos = Grupo::all();
+                $evolutionApi = new EvolutionApi();
+                $ofertaService = new OfertaService();
+    
+                $mensagem = $ofertaService->formatMessage($produto->toArray());
+        
                 foreach ($grupos as $grupo) {
-                    $this->ofertaService->sendText($grupo->grupo_id, $mensagem);
-
-                }                
+                    try {
+                        
+                        $resposta = $evolutionApi->sendText($grupo->grupo_id, $mensagem);
+                        info($resposta);
+    
+                    } catch (Exception $e) {
+                        Log::error("Falha ao enviar mensagem para o grupo {$grupo->grupo_id}: " . $e->getMessage());
+                        
+                        break; 
+                    }
+                    sleep(4);
+                }            
                 // event(new ProdutoCriado($produto));
             }
             
