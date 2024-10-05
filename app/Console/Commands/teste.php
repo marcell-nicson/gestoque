a4229848016e58092762aa4ae4e70615b7c44abf
Marcell Nicson <marcell.nicson@gmail.com>
2024-10-05 16:10:50 -0300

    iniciar

--- app/Console/Commands/teste.php
+++ app/Console/Commands/teste.php
@@ -9,9 +9,16 @@ use App\Models\Marca;
 use App\Models\Produto;
 use App\Models\Saida;
 use App\Models\Venda;
+use App\Services\EvolutionApi;
+use App\Services\MercadoLivreApi;
+use App\Services\OfertaService;
 use App\Services\Uzapi;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Redis;
 
 use function App\Helpers\calculoEntradaeSaida;
 
@@ -37,36 +44,120 @@ class teste extends Command
     public function handle()
     {
 
+
+
         try {
+            $service = new MercadoLivreApi();
 
-            // $uzapi = new Uzapi();
 
-            // $grupoId = '120363303397548933';
-            // $numeros = [
-            //     '558496377856',
-            //     '558481345425',
-            //     '558496539798',
-            // ];
 
-            // foreach ($numeros as $numero) {
+            $app_id = '4086879977097318';
+            $client_secret = 'y0T5Qs1BA6tKZLwkzk0psVEmrWBJ5VEe';
+            $url_principal = 'https://api.mercadolibre.com/oauth/token';
 
-            //     $resposta = $uzapi->addParticipantGroup($grupoId, $numero);
-            //     if($resposta){
-            //         $this->info('adicionado: '. $numero);
-            //     }
-            // }
+            $data = [
+                'grant_type' => 'refresh_token',
+                'client_id' => $app_id,
+                'client_secret' => $client_secret,
+                'refresh_token' => 'TG-67017ec1e418f5000186632a-130970326',
+            ];
+    
+            $response = Http::asForm()->post($url_principal, $data);
+            $resposta = $response->json();
+
+            $refresh_token_key = 'ml_refresh_token'; 
+            $access_token_key = 'ml_access_token';  
+            $expires_at_key = 'ml_token_expires_at'; 
+
+            $expires_in = $resposta['expires_in'];
+            $new_refresh_token = $resposta['refresh_token'];
+            $access_token = $resposta['access_token'];
+            $token_expires_at = Carbon::now()->addSeconds($expires_in);
+
+            Redis::setex($access_token_key, $expires_in, $access_token);
+            Redis::setex($refresh_token_key, $expires_in, $new_refresh_token);
+            Redis::setex($expires_at_key, $expires_in, $token_expires_at->toISOString());
+
+
+            // $service = new MercadoLivreApi();
+            // $ofertaService = new OfertaService();
+            // $evolutionApi = new EvolutionApi();
+      
+                      
+
+            //     $jsonResponse = $service->getOfertas();
+            //     $json = $jsonResponse->getData(true);        
+ 
             
-            $grupo = new Grupo();
-            $grupo->nome = 'Nome do Grupo';
-            $grupo->grupo_id = '123456789';
-            $grupo->descricao = 'Descrição do grupo';
-            $grupo->save();
+            //     $filtradas = [];
+            //     foreach ($json['results'] as $result) {
+            //         $filtradas[] = [
+            //             'id' => $result['id'],
+            //             'nome' => $result['title'],
+            //             'category_id' => $result['category_id'],
+            //             'thumbnail' => $result['thumbnail'],
+            //             'valor' => $result['price'],
+            //             'original_price' => $result['original_price'],
+            //             'image' => 'http://http2.mlstatic.com/D_NQ_NP_' . $result['thumbnail_id'] . '-O.webp'
+            //         ];
 
+            //         $criado = $ofertaService->storeOferta($filtradas);
+            //         $mensagem = $ofertaService->formatMessage($criado);
+                    
+            //         $grupos = Grupo::all();
+
+            //         foreach ($grupos as $grupo) {
+            //             try {
+                            
+            //                 $resposta = $evolutionApi->sendText($grupo->grupo_id, $mensagem);
+            //                 info($resposta);
+            //                 dd($resposta);
+            //             } catch (Exception $e) {
+            //                 Log::error("Falha ao enviar mensagem para o grupo {$grupo->grupo_id}: " . $e->getMessage());
+                            
+            //                 break; 
+            //             }
+            //             sleep(4);
+            //         }  
+            //     }            
+          
+                // event(new ProdutoCriado($produto));
+            
+            
+            
         } catch (Exception $e) {
             $this->info($e->getMessage());
-        }
-
-            
+        }            
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
