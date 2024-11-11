e2abbd17eb911af263f63ca53ee069b7a95125eb
Marcell Nicson <marcell.nicson@gmail.com>
2024-11-11 08:28:38 -0300

    a

--- app/Console/kernel.php
+++ app/Console/kernel.php
@@ -11,7 +11,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('envio:produtos-apimercadolivre')->everyFiveMinutes();
         //A cada uma hora
-        $schedule->command('app:enviar-produto-pendente')->everyTwoMinutes();
+        $schedule->command('app:enviar-produto-pendente')->everyMinute();
 
     }
     protected function commands()
