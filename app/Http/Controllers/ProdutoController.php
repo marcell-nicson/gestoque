c2670fc8eb9b8eab788967938c3a6d5c337b81b7
Marcell Nicson <marcell.nicson@gmail.com>
2024-11-29 16:44:03 -0300

    a

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -158,24 +158,27 @@ class ProdutoController extends Controller
 
                 $grupo_geral = false;
                 $grupo_categoria = false;
-    
-                if($grupo->grupo_id == '120363303397548933@g.us' && $produto->categoria_id === null){
+        
+                if($grupo->nome == 'G-Ofertas #1' && $produto->categoria_id === null or isset($produto->categoria_id) && $grupo->nome == 'G-Ofertas #1'){
                     $grupo_geral = true;
-                }
-    
-                if(isset($produto->categoria_id) && $grupo->grupo_id == '120363303397548933@g.us'){
-                    $grupo_geral = true;
-                }
+                } 
                 
                 if (isset($produto->categoria_id) && isset($grupo->categoria_id) && $produto->categoria_id == $grupo->categoria_id) {
                     $grupo_categoria = true;
                 }
-    
+        
                 if ($grupo_categoria || $grupo_geral) {
-                    $evolutionApi->sendText($grupo->grupo_id, $mensagem);
-                    $produto->status = 'enviado';
-                    $produto->save(); 
-                }
+        
+                    if($evolutionApi->sendText($grupo->grupo_id, $mensagem)){                
+                    
+                        $produto->status = 'enviado';
+                        $produto->save(); 
+        
+                        return true;
+                    }         
+        
+                } 
+                
 
             } catch (Exception $e) {
                 Log::error("Falha ao enviar mensagem para o grupo {$grupo->grupo_id}: " . $e->getMessage());
